d35ab716cb6a1e0799c98d1f5f3fcb10637aac98
pierobassa <pierobassa222@gmail.com>
2022-02-23 20:41:26 +0100

    Initial push

---
+++ ball-sort-master/src/tube/Ball.java
@@ -0,0 +1,11 @@
+package tube;
+
+import java.awt.*;
+
+public class Ball { //Id needed to identify the balls for move actions
+    public Color color;
+
+    public Ball(Color color) {
+        this.color = color;
+    }
+}
