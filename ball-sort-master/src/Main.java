6a65a669a55247a79254d0973592c8db1b09600a
pierobassa <pierobassa222@gmail.com>
2022-02-26 15:09:52 +0100

    Initial test java proj

--- ball-sort-master/src/Main.java
+++ ball-sort-master/src/Main.java
@@ -1,6 +1,11 @@
 import javax.swing.*;
 
 import aspmodel.Boccetta;
+import aspmodel.Colore;
+import aspmodel.Move;
+import aspmodel.On;
+import aspmodel.Pallina;
+import aspmodel.Tempo;
 import it.unical.mat.embasp.base.Handler;
 import it.unical.mat.embasp.base.InputProgram;
 import it.unical.mat.embasp.base.Output;
@@ -8,6 +13,7 @@ import it.unical.mat.embasp.languages.IllegalAnnotationException;
 import it.unical.mat.embasp.languages.ObjectNotValidException;
 import it.unical.mat.embasp.languages.asp.ASPInputProgram;
 import it.unical.mat.embasp.languages.asp.ASPMapper;
+import it.unical.mat.embasp.languages.asp.AnswerSet;
 import it.unical.mat.embasp.languages.asp.AnswerSets;
 import it.unical.mat.embasp.platforms.desktop.DesktopHandler;
 import it.unical.mat.embasp.specializations.dlv2.desktop.DLV2DesktopService;
@@ -40,10 +46,11 @@ public class Main {
       	//classi Boccetta,Color, On, Pallina, Tempo che vengono prima registrate all'ASPMapper
       	try {
 			ASPMapper.getInstance().registerClass(Boccetta.class);
-//			ASPMapper.getInstance().registerClass(aspmodel.Color.class);
-//			ASPMapper.getInstance().registerClass(On.class);
-//			ASPMapper.getInstance().registerClass(Pallina.class);
-//			ASPMapper.getInstance().registerClass(Tempo.class);
+			ASPMapper.getInstance().registerClass(Colore.class);
+			ASPMapper.getInstance().registerClass(On.class);
+			ASPMapper.getInstance().registerClass(Pallina.class);
+			ASPMapper.getInstance().registerClass(Tempo.class);
+			ASPMapper.getInstance().registerClass(Move.class);
 		} catch (ObjectNotValidException | IllegalAnnotationException e1) {
 			e1.printStackTrace();
 		}
@@ -51,7 +58,43 @@ public class Main {
       	InputProgram facts = new ASPInputProgram();
       	
       	try {
-			facts.addObjectInput(new Boccetta(1));
+      		for(int i = 1; i <= 5; i++) {
+				facts.addObjectInput(new Boccetta(i));
+			}
+			
+			facts.addObjectInput(new Colore("rosso"));
+			facts.addObjectInput(new Colore("blu"));
+			facts.addObjectInput(new Colore("verde"));
+			
+			for(int i = 1; i <= 11; i++) {
+				facts.addObjectInput(new Tempo(i));
+			}
+			
+			facts.addObjectInput(new On(1,0,1,0));
+			facts.addObjectInput(new On(2,0,2,0));
+			facts.addObjectInput(new On(3,0,3,0));
+			facts.addObjectInput(new On(4,1,1,0));
+			facts.addObjectInput(new On(5,2,2,0));
+			facts.addObjectInput(new On(6,3,3,0));
+			facts.addObjectInput(new On(7,4,1,0));
+			facts.addObjectInput(new On(8,5,2,0));
+			facts.addObjectInput(new On(9,6,3,0));
+			facts.addObjectInput(new On(10,7,1,0));
+			facts.addObjectInput(new On(11,8,2,0));
+			facts.addObjectInput(new On(12,9,3,0));
+			
+			facts.addObjectInput(new Pallina(1, "blu"));
+			facts.addObjectInput(new Pallina(2, "verde"));
+			facts.addObjectInput(new Pallina(3, "blu"));
+			facts.addObjectInput(new Pallina(4, "verde"));
+			facts.addObjectInput(new Pallina(5, "rosso"));
+			facts.addObjectInput(new Pallina(6, "rosso"));
+			facts.addObjectInput(new Pallina(7, "blu"));
+			facts.addObjectInput(new Pallina(8, "rosso"));
+			facts.addObjectInput(new Pallina(9, "verde"));
+			facts.addObjectInput(new Pallina(10, "verde"));
+			facts.addObjectInput(new Pallina(11, "blu"));
+			facts.addObjectInput(new Pallina(12, "rosso"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -70,10 +113,23 @@ public class Main {
 		// i fatti
 		Output o = handler.startSync();
 
-		// Analizziamo l'answer set che in quest caso è unico e che rappresenta la
-		// soluzione
-		// del Sudoku e aggiorniamo la matrice
+		// Analizziamo l'answer set 
 		AnswerSets answersets = (AnswerSets) o;
+		for(AnswerSet a:answersets.getAnswersets()){
+			try {
+				for(Object obj:a.getAtoms()){
+					//Scartiamo tutto ciò che non è un oggetto della classe Cell
+					if(!(obj instanceof Move)) continue;
+					//Convertiamo in un oggetto della classe Cell e impostiamo il valore di ogni cella 
+					//nella matrice rappresentante la griglia del Sudoku
+					Move move= (Move) obj;	
+					System.out.println("move (" + move.getPallina() + " " + move.getBoccetta() + " " + move.getTempo() + ")");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+			
+		}
     }
 
 }
--- ball-sort-master/src/aspmodel/Colore.java
+++ ball-sort-master/src/aspmodel/Colore.java
@@ -1,21 +1,26 @@
-//package aspmodel;
-//
-//import it.unical.mat.embasp.languages.Id;
-//import it.unical.mat.embasp.languages.Param;
-//
-//@Id("color")
-//public class Colore {
-//
-//	@Param(0)
-//	private String colore;
-//	
-//	public Colore() {}
-//
-//	public String getColore() {
-//		return colore;
-//	}
-//
-//	public void setColore(String colore) {
-//		this.colore = colore;
-//	}
-//}
+package aspmodel;
+
+import it.unical.mat.embasp.languages.Id;
+import it.unical.mat.embasp.languages.Param;
+
+@Id("color")
+public class Colore {
+
+	@Param(0)
+	private String colore;
+	
+	public Colore() {}
+	
+	public Colore(String colore) {
+		this.colore = colore;
+	}
+
+
+	public String getColore() {
+		return colore;
+	}
+
+	public void setColore(String colore) {
+		this.colore = colore;
+	}
+}
